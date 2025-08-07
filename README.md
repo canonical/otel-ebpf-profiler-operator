ae036fc2ea1e96dc8e36ecf396b45cb834259ab0
michael <michael.dmitry98@gmail.com>
2025-08-07 13:20:20 +0300

    review base: wiped all files

--- README.md
+++
@@ -1,3 +0,0 @@
-# otel-ebpf-profiler-operator
-
-Deploys and operates https://github.com/canonical/otel-ebpf-profiler-snap on machine Juju models.
--- src/constants.py
+++
@@ -1,6 +0,0 @@
-from typing import Final
-
-SERVER_CERT_PATH: Final[str] = (
-    "/usr/local/share/ca-certificates/juju_tls-certificates/otlp_ebpf_profiler-server.crt"
-)
-SERVER_CERT_PRIVATE_KEY_PATH: Final[str] = "/etc/otlp_ebpf_profiler/private.key"
