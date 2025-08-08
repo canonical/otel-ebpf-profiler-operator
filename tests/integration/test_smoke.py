f07ab4bdc40e3c4b9dae37c26aea055f81e49b6f
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-08-08 16:15:36 +0200

    fixed sideload script

--- tests/integration/test_smoke.py
+++ tests/integration/test_smoke.py
@@ -15,16 +15,7 @@ def test_sideload_snap(juju: Juju, tmp_path):
     # FIXME: https://github.com/canonical/otel-ebpf-profiler-operator/issues/3
     unit_name = list(juju.status().apps[APP_NAME].units.keys())[0]
 
-    sideload_snap(tmp_path, unit_name)
-    # this is basically jhack fire install
-    juju.ssh(
-        unit_name,
-        f"sudo /usr/bin/juju-exec -u {unit_name} "
-        "JUJU_DISPATCH_PATH=hooks/install "
-        f"JUJU_MODEL_NAME={juju.model} "
-        f"JUJU_UNIT_NAME={unit_name} "
-        f"/var/lib/juju/agents/unit-{unit_name.replace('/', '-')}/charm/dispatch",
-    )
+    sideload_snap(juju, tmp_path, unit_name)
 
     juju.wait(jubilant.all_active)
 
