a564872bb8587daca871482ba80d13241cee68cb
michael <michael.dmitry98@gmail.com>
2025-08-15 13:32:17 +0300

    chore: PR feedback

--- tests/integration/test_self_monitoring.py
+++ tests/integration/test_self_monitoring.py
@@ -18,7 +18,7 @@ METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_BASE = next(iter(METADATA["platforms"])).split(":")[0]
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", autouse=True)
 def patch_update_status_interval(juju: Juju):
     # patch the update-status-hook-interval because if the otel collector charm handles an event, it will regenerate its config and overwrite the config patch
     juju.model_config({"update-status-hook-interval": "1h"})
@@ -27,26 +27,23 @@ def patch_update_status_interval(juju: Juju):
 
 
 def _patch_otel_collector_log_level(juju: Juju, unit_no=0):
-    # patch the collector's log level to INFO
+    # patch the collector's log level to INFO; we need this so that we can inspect the telemetry being dumped by the `debug` exporter
     juju.ssh(
         f"{OTEL_COLLECTOR_APP_NAME}/{unit_no}",
         f"sudo sed -i 's/level: WARN/level: INFO/' /etc/otelcol/config.d/{OTEL_COLLECTOR_APP_NAME}_{unit_no}.yaml",
     )
-    # resatart the snap for the updates to take place
+    # restart the snap for the updates to take place
     juju.ssh(f"{OTEL_COLLECTOR_APP_NAME}/{unit_no}", "sudo snap restart opentelemetry-collector")
 
 
-def _is_pattern_in_snap_logs(juju: Juju, grep_filters: List[str]):
+def assert_pattern_in_snap_logs(juju: Juju, grep_filters: List[str]):
     cmd = (
         "sudo snap logs opentelemetry-collector -n=all"
         + " | "
         + " | ".join([f"grep {p}" for p in grep_filters])
     )
     otelcol_logs = juju.ssh(f"{OTEL_COLLECTOR_APP_NAME}/0", command=cmd)
-
-    if not otelcol_logs:
-        raise Exception(f"Filters {grep_filters} not found in the {OTEL_COLLECTOR_APP_NAME} logs")
-    return True
+    assert otelcol_logs, f"Filters {grep_filters} not found in the {OTEL_COLLECTOR_APP_NAME} logs"
 
 
 @pytest.mark.setup
@@ -70,7 +67,7 @@ def test_deploy_and_integrate_otel_collector(juju: Juju):
 
 @pytest.mark.setup
 @when("integrated with the otel-ebpf-profiler over cos-agent")
-def test_integrate_cos_agent(juju: Juju, patch_update_status_interval):
+def test_integrate_cos_agent(juju: Juju):
     juju.integrate(APP_NAME + ":cos-agent", OTEL_COLLECTOR_APP_NAME + ":cos-agent")
     juju.wait(
         lambda status: jubilant.all_blocked(status, OTEL_COLLECTOR_APP_NAME),
@@ -97,14 +94,14 @@ def test_logs_are_scraped(juju: Juju):
         "log.file.name=otel-ebpf-profiler.log",
         "log.file.path=/var/log/otel-ebpf-profiler.log",
     ]
-    assert _is_pattern_in_snap_logs(juju, grep_filters)
+    assert_pattern_in_snap_logs(juju, grep_filters)
 
 
 @then("metrics are being scraped by the collector")
 @retry(stop=stop_after_attempt(10), wait=wait_fixed(10))
 def test_metrics_are_scraped(juju: jubilant.Juju):
     grep_filters = [f"juju_application={APP_NAME}", f"juju_model={juju.model}"]
-    assert _is_pattern_in_snap_logs(juju, grep_filters)
+    assert_pattern_in_snap_logs(juju, grep_filters)
 
 
 @then("the collector aggregates the profiler's log alert rules")
