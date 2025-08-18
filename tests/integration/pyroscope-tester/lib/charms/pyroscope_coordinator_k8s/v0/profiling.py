0d0eaba9f5fbf0f7b948ff3c3bdbc4629eced522
michael <michael.dmitry98@gmail.com>
2025-08-18 14:15:01 +0300

    fix: update lib in tester

--- tests/integration/pyroscope-tester/lib/charms/pyroscope_coordinator_k8s/v0/profiling.py
+++ tests/integration/pyroscope-tester/lib/charms/pyroscope_coordinator_k8s/v0/profiling.py
@@ -1,5 +1,5 @@
-"""Profiling integration endpoint wrapper."""
-
+"""Profiling integration endpoint wrapper.
+"""
 import dataclasses
 import logging
 from typing import List
@@ -15,38 +15,37 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 DEFAULT_ENDPOINT_NAME = "profiling"
 
 logger = logging.getLogger()
 
-
 class ProfilingAppDatabagModel(pydantic.BaseModel):
     """Application databag model for the profiling interface."""
-
     otlp_grpc_endpoint_url: str
+    insecure: bool = False
 
 
 class ProfilingEndpointProvider:
     """Wraps a profiling provider endpoint."""
-
-    def __init__(self, relations: List[ops.Relation], app: ops.Application):
+    def __init__(self, relations:List[ops.Relation], app:ops.Application):
         self._relations = relations
         self._app = app
 
-    def publish_endpoint(
-        self,
-        otlp_grpc_endpoint: str,
-    ):
+    def publish_endpoint(self,
+                         otlp_grpc_endpoint:str,
+                         insecure:bool=False,
+                         ):
         """Publish profiling ingestion endpoints to all relations."""
         for relation in self._relations:
             try:
                 relation.save(
                     ProfilingAppDatabagModel(
                         otlp_grpc_endpoint_url=otlp_grpc_endpoint,
+                        insecure=insecure,
                     ),
-                    self._app,
+                    self._app
                 )
             except ops.ModelError:
                 logger.debug("failed to validate app data; is the relation still being created?")
@@ -56,30 +55,29 @@ class ProfilingEndpointProvider:
 @dataclasses.dataclass
 class _Endpoint:
     otlp_grpc: str
+    insecure: bool = False
 
 
 class ProfilingEndpointRequirer:
     """Wraps a profiling requirer endpoint."""
-
-    def __init__(self, relations: List[ops.Relation]):
+    def __init__(self, relations:List[ops.Relation]):
         self._relations = relations
 
-    def get_endpoints(self) -> List[_Endpoint]:
+    def get_endpoints(self)->List[_Endpoint]:
         """Obtain the profiling endpoints from all relations."""
         out = []
         for relation in self._relations:
             try:
                 data = relation.load(ProfilingAppDatabagModel, relation.app)
-                otlp_grpc_endpoint_url = data.otlp_grpc_endpoint_url
             except ops.ModelError:
                 logger.debug("failed to validate app data; is the relation still being created?")
                 continue
             except pydantic.ValidationError:
                 logger.debug("failed to validate app data; is the relation still settling?")
                 continue
-            out.append(
-                _Endpoint(
-                    otlp_grpc=otlp_grpc_endpoint_url,
-                )
-            )
+            out.append(_Endpoint(
+                otlp_grpc=data.otlp_grpc_endpoint_url,
+                insecure=data.insecure,
+            ))
         return out
+
